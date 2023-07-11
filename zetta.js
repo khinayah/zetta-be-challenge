ccb92f992fac94615b0cf6c5bd0824960ef25d24
Linda Kh <kh.inayah11@gmail.com>
2023-07-11 11:07:23 +0700

    first commit

---
+++ zetta.js
@@ -0,0 +1,21 @@
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+]
+
+function result(param) {
+  let maxWords = 0
+
+  for (let i = 0; i < param.length; i++) {
+    const words = param[i].split(' ')
+    const wordCount = words.length
+
+    if (wordCount > maxWords) {
+      maxWords = wordCount
+    }
+  }
+  return maxWords
+}
+
+console.log(result(sentences))
